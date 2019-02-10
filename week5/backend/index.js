3c4acf6ad6e697bce005dffe77bd217ae10b26e5
adarshsharma.cs <adarshsharma.cs@gmail.com>
2019-02-10 14:33:50 +0000

    Update 'week5/backend/index.js'

--- week5/backend/index.js
+++ week5/backend/index.js
@@ -1,6 +1,6 @@
 ////insert data into our running mongodb (running in container)
 const MongoClient = require("mongodb").MongoClient;
-const DBName = "entertainment";
+const DBName = "The90sCartoons";
 
 //note authentication via url
 //this should be replaced with environment variables (for security)
@@ -56,70 +56,6 @@ const CartoonData = [
     SimilarSeries:["Rocko's Modern Life", "Cat Dog", "hey Arnold!", "The Fairly Odd Parents"] 
   }
 ];
-  {
-    firstName: "John",
-    lastName: "Travolta",
-    birth: "February 18, 1954",
-    credits: 76,
-    tags: ["actor", "soundtrack", "producer"],
-    image:
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUwNjQ0ODkxN15BMl5BanBnXkFtZTcwMDc5NjQwNw@@._V1_SY1000_CR0,0,743,1000_AL_.jpg",
-    movies: [
-      {
-        title: "Battlefield Earth",
-        year: 2000
-      },
-      {
-        title: "FaceOff",
-        year: 1997
-      },
-      {
-        title: "Pulp Fiction",
-        year: 1994
-      }
-    ]
-  },
-  {
-    firstName: "Uma",
-    lastName: "Thurman",
-    birth: "April 29, 1970",
-    tags: ["actress", "soundtrack", "writer"],
-    image:
-      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjMxNzk1MTQyMl5BMl5BanBnXkFtZTgwMDIzMDEyMTE@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
-    credits: 63,
-    awards: [
-      {
-        type: "oscar",
-        status: "nominated",
-        title: "best actress",
-        year: 1995,
-        film: "Pulp Fiction"
-      },
-      {
-        type: "golden globe",
-        status: "nominated",
-        title: "best performance",
-        year: 2005,
-        film: "Kill Bill Vol2"
-      }
-    ],
-    movies: [
-      {
-        title: "Kill Bill Vol1",
-        year: 2003
-      },
-      {
-        title: "Pulp Fiction",
-        year: 1994
-      },
-      {
-        title: "Batman and Robin",
-        year: 1994
-      }
-    ]
-  }
-];
-
 //connect call returns a connection promise, which can be 'then' with a callback
 MongoClient.connect(url)
   .then(con => {
